cfd823478526440d7651c5b7796dc5e67e5ade3d
hsalucamba <hsalucamba@interdigitos.co.ao>
2021-10-25 12:00:35 +0100

    :walking: Understanding indentation

---
+++ Module 1/indentation.py
@@ -0,0 +1,20 @@
+# Code Indetation in Python
+
+import time
+
+def PensaPor10Segundos():
+    print('pensando')
+    time.sleep(10)
+    print('Lembrei')
+
+if 10 > 5:
+    print('10 é maior que 5')
+
+class BemVindo():
+    def __init__(self):
+        print('Bem vindo')
+
+
+oi = BemVindo()
+
+PensaPor10Segundos()
