c62c0ac3029b5c8513a90e56e02111ab71c9ef07
hsalucamba <hsalucamba@interdigitos.co.ao>
2021-10-25 11:22:39 +0100

    :walking: Understanding variables

---
+++ Module 1/variables.py
@@ -0,0 +1,31 @@
+# Creating variables in Python (Allocating memory)
+velocidade_internet = 10
+print(velocidade_internet)
+
+# Primitive Data Types
+# Integer
+idade = 10
+
+# Boolean True | False
+maior_idade = False
+
+# Strings
+slogan = 'Feito maior que perfeito'
+
+# Unpacking
+ab, cd, ef = 2, 4, 'Testando unpack de variáveis'
+
+
+#Identifying variable Types
+preco = 25.26
+disponivel = True
+disconto = 20
+boas_vindas = 'Bem-vindo a nossa loja.'
+
+
+print(type(preco))
+print(type(disponivel))
+print(type(disconto))
+print(type(boas_vindas))
+
+
