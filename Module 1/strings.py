d857be7fcb23b1d61a22b103b3040dc8784255f8
hsalucamba <hsalucamba@interdigitos.co.ao>
2021-10-25 12:21:25 +0100

    :walking: Understanding strings

---
+++ Module 1/strings.py
@@ -0,0 +1,21 @@
+# Understanding strings
+
+# Strings can be declared using single quotes '' or double quotes "" 
+# or triple single quotes for more than one paragraph (''') or (""")
+
+print('Hi my name in Python single quotes.')
+print("Hi, my name is Python in double quotes.")
+print("""Hi, my name is Python in triple quotes.
+I can be used to print more than one line in a single statement or variable
+""")
+
+# Escaping characters
+print('Hi, I can be used to escape some words. \nCall me Mr Special. 😎')
+print('I can be used to ignore some characters, I\'am ignored\n')
+
+path = 'C:\\Users\\henriques.salucamba\\Documents\\_labs'
+
+# Comment shortcuts command
+# CTRL + K + C = single line comment
+# CTRL + K + U = single line uncomment 
+# SHIFT + ALT + A = multiline line uncomment 
